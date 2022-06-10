16729dc2161cbb8ee2e7b88f1ed0b33bcfedf6a0
Yaasir024 <yope4ever@gmail.com>
2022-06-10 17:55:00 +0100

    /created footer

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
-
 <template>
-
   <Nav />
   <RouterView />
+  <Footer />
 </template>
 
 <script>
-import { RouterLink, RouterView } from 'vue-router'
-import Nav from '@/components/Nav.vue'
+import { RouterLink, RouterView } from "vue-router";
+import Nav from "@/components/Nav.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   components: {
     Nav,
-  }
-}
+    Footer,
+  },
+};
 </script>
 
-
-
 <style>
-@import '@/assets/base.css';
-
-
+@import "@/assets/base.css";
 </style>
