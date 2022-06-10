da56191b8fbf1f10c18a620de62bd71072f1b79e
Yaasir024 <yope4ever@gmail.com>
2022-06-10 13:22:17 +0100

    /Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,13 @@
-
 <template>
-  Home
+  <main>
+    <div class="section-hero"></div>
+  </main>
 </template>
+
+<style scoped>
+.section-hero {
+  background: linear-gradient(#020d1f 16%, black 35%);
+  min-height: 1160px;
+  color: #f5f5f7;
+}
+</style>
